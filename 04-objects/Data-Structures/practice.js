4f0b2a4f6d32fcb9d7556b9be51a5fb5bdab5625
justBotir <raimboyevb@gmail.com>
2026-03-28 10:35:36 +0500

    Set and Map practice

--- 04-objects/Data-Structures/practice.js
+++ 04-objects/Data-Structures/practice.js
@@ -1,73 +1,73 @@
-// // 1. Massiv ichidan birinchi uchragan juft sonni aniqlang. Agar topilmasa, undefined qaytsin.
+// 1. Massiv ichidan birinchi uchragan juft sonni aniqlang. Agar topilmasa, undefined qaytsin.
 
-// const arr = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+const arr = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
 
-// const result = arr.find((item) => item.id % 2 === 0);
+const result = arr.find((item) => item.id % 2 === 0);
 
-// console.log(result);
+console.log(result);
 
-// // 2. Massiv ichidan birinchi uchragan musbat sonning indeksini qaytaring. Agar bunday son bo'lmasa, -1 chiqsin.
+// 2. Massiv ichidan birinchi uchragan musbat sonning indeksini qaytaring. Agar bunday son bo'lmasa, -1 chiqsin.
 
-// const arr1 = [{ id: -1 }, { id: -1 }, { id: -2 }, { id: -3 }, { id: -2 }];
+const arr1 = [{ id: -1 }, { id: -1 }, { id: -2 }, { id: -3 }, { id: -2 }];
 
-// const result1 = arr1.findIndex((item) => item.id >= 0);
-// console.log(result1);
+const result1 = arr1.findIndex((item) => item.id >= 0);
+console.log(result1);
 
-// // 3. Massiv ichidagi oxirgi uchragan toq sonning indeksini toping. Agar bunday son bo'lmasa, -1 chiqsin.
+// 3. Massiv ichidagi oxirgi uchragan toq sonning indeksini toping. Agar bunday son bo'lmasa, -1 chiqsin.
 
-// const arr2 = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+const arr2 = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
 
-// const result2 = arr2.findLastIndex((item) => item.id % 2 !== 0);
-// console.log(result2);
+const result2 = arr2.findLastIndex((item) => item.id % 2 !== 0);
+console.log(result2);
 
-// // 4. Massiv ichidan faqat 0 dan katta elementlarni ajratib oling va yangi massiv qaytaring.
+// 4. Massiv ichidan faqat 0 dan katta elementlarni ajratib oling va yangi massiv qaytaring.
 
-// const arr3 = [-3, -2, -1, 0, 1, 2, 3];
+const arr3 = [-3, -2, -1, 0, 1, 2, 3];
 
-// const result3 = arr3.filter((item) => item > 0);
-// console.log(result3);
+const result3 = arr3.filter((item) => item > 0);
+console.log(result3);
 
-// // 5. Massiv elementlarining kvadratlarini hisoblab, yangi massiv hosil qiling.
+// 5. Massiv elementlarining kvadratlarini hisoblab, yangi massiv hosil qiling.
 
-// const arr4 = [1, 2, 3, 4, 5];
+const arr4 = [1, 2, 3, 4, 5];
 
-// const result4 = arr4.map((item) => item ** 2);
-// console.log(result4);
+const result4 = arr4.map((item) => item ** 2);
+console.log(result4);
 
-// // 6. Berilgan massivni o'sish tartibida tartiblang.
+// 6. Berilgan massivni o'sish tartibida tartiblang.
 
-// const arr5 = [1, 4, 2, 6, 8, 3, 10, 9, 5, 7];
+const arr5 = [1, 4, 2, 6, 8, 3, 10, 9, 5, 7];
 
-// const result5 = arr5.sort((a, b) => a - b);
-// console.log(result5);
+const result5 = arr5.sort((a, b) => a - b);
+console.log(result5);
 
-// // 7. Massiv elementlarini orqaga qarab joylashtiring.
+// 7. Massiv elementlarini orqaga qarab joylashtiring.
 
-// const arr6 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr6 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// const result6 = arr6.reverse();
-// console.log(result6);
+const result6 = arr6.reverse();
+console.log(result6);
 
-// // 8. Berilgan gapni har bir so‘zini alohida massiv elementiga ajratib oling.
+// 8. Berilgan gapni har bir so‘zini alohida massiv elementiga ajratib oling.
 
-// const str = "Assalomu alaykum";
+const str = "Assalomu alaykum";
 
-// const result7 = str.split(" ");
-// console.log(result7);
+const result7 = str.split(" ");
+console.log(result7);
 
-// // 9. Massiv elementlarini bo‘sh joy bilan ajratib, bitta matn sifatida chiqarib bering.
+// 9. Massiv elementlarini bo‘sh joy bilan ajratib, bitta matn sifatida chiqarib bering.
 
-// const arr7 = ["Assalomu", "Alaykum", "Hammaga"];
+const arr7 = ["Assalomu", "Alaykum", "Hammaga"];
 
-// const result8 = arr7.join(" ");
-// console.log(result8);
+const result8 = arr7.join(" ");
+console.log(result8);
 
-// // 10. Berilgan sonlar massivining yig‘indisini toping.
+// 10. Berilgan sonlar massivining yig‘indisini toping.
 
-// const arr8 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr8 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// const result9 = arr8.reduce((acc, item) => acc + item, 0);
-// console.log(result9);
+const result9 = arr8.reduce((acc, item) => acc + item, 0);
+console.log(result9);
 
 // ! Map bilan ishlash misollari
 /* 
@@ -120,6 +120,17 @@ console.log(products.size);
 5.clear yordamida barcha elementlarni o‘chiring.
 */
 
+const numbers = new Set();
+
+numbers.add(1).add(2).add(3).add(4).add(5).add(6);
+console.log(numbers);
+console.log(numbers.has(5));
+numbers.delete(2);
+console.log(numbers);
+console.log(numbers.size);
+numbers.clear();
+console.log(numbers);
+
 /*
 * 2. Takrorlanmagan ismlar ro‘yxatini Set yordamida tuzing va quyidagi amallarni bajaring:
 1.add yordamida 4 ta ism qo‘shing.
@@ -127,3 +138,12 @@ console.log(products.size);
 3.forEach yordamida barcha ismlarni ekranga chiqaring.
 4.size bilan umumiy sonni ko‘rsating.
 */
+
+const names = new Set();
+
+names.add("Ali").add("Vali").add("Javohir").add("Behuz");
+console.log(names.has("Ali"));
+const allNames = names.forEach((values, keys, names) => {
+  console.log(keys);
+});
+console.log(names.size);
